a52bc90f62b6795d039aa51fb8dcb9a07c7122f6
secondman <secondman@jetstreamlabs.com>
2025-11-19 06:48:41 -0700

    fix: update release config and ci
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- release.config.js
+++ release.config.js
@@ -20,6 +20,8 @@ const commitAnalyzerOptions = {
 const releaseNotesGeneratorOptions = {
 	writerOpts: {
 		transform: (commit, context) => {
+			// Create a mutable copy of the commit object
+			const modifiedCommit = { ...commit }
 			const issues = []
 
 			const types = {
@@ -32,10 +34,10 @@ const releaseNotesGeneratorOptions = {
 				chore: 'Maintenance',
 			}
 
-			commit.type = types[commit.type]
+			modifiedCommit.type = types[commit.type]
 
 			if (typeof commit.hash === 'string') {
-				commit.shortHash = commit.hash.substring(0, 7)
+				modifiedCommit.shortHash = commit.hash.substring(0, 7)
 			}
 
 			if (typeof commit.subject === 'string') {
@@ -43,25 +45,28 @@ const releaseNotesGeneratorOptions = {
 				if (url) {
 					url = `${url}/issues/`
 					// Issue URLs.
-					commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+					modifiedCommit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
 						issues.push(issue)
 						return `[#${issue}](${url}${issue})`
 					})
 				}
 				if (context.host) {
 					// User URLs.
-					commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
-						if (username.includes('/')) {
-							return `@${username}`
-						}
+					modifiedCommit.subject = modifiedCommit.subject.replace(
+						/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g,
+						(_, username) => {
+							if (username.includes('/')) {
+								return `@${username}`
+							}
 
-						return `[@${username}](${context.host}/${username})`
-					})
+							return `[@${username}](${context.host}/${username})`
+						}
+					)
 				}
 			}
 
 			// remove references that already appear in the subject
-			commit.references = commit.references.filter((reference) => {
+			modifiedCommit.references = commit.references.filter((reference) => {
 				if (issues.indexOf(reference.issue) === -1) {
 					return true
 				}
@@ -69,7 +74,7 @@ const releaseNotesGeneratorOptions = {
 				return false
 			})
 
-			return commit
+			return modifiedCommit
 		},
 	},
 }
